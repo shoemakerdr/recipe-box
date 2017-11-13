dfda60045e255b745577f6969f6414c2e8c1f399
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-13 22:04:27 +0000

    Added react-router-dom and import child components.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import './App.css'
+import RecipeList from './RecipeList'
+import RecipeModal from './RecipeModal'
+import EditModal from './EditModal'
+import Store from './store'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor () {
+        super()
+        this.recipeStore = new Store()
+    }
+
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <div className="App-header">
+                        <h2>Recipe Box</h2>
+                    </div>
+                    <RecipeList />
+                    <Route path='/:recipe' render={props => (
+                        <RecipeModal {...props} />
+                    )} />
+                    <Route path='/:recipe/edit' render={props => (
+                        <EditModal {...props} />
+                    )} />
+                </div>
+            </Router>
+        )
+    }
 }
 
-export default App;
+export default App
