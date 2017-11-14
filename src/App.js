62e3a97e005fe94a3feebab402b0ddcd33ceda92
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-14 21:08:47 +0000

    Added Modal component. Now wrapping RecipeCard and EditCard components with Modal.Moved store configuration to store-config.

--- src/App.js
+++ src/App.js
@@ -2,30 +2,33 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css'
 import RecipeList from './RecipeList'
-import RecipeModal from './RecipeModal'
-import EditModal from './EditModal'
-import Store from './store'
+import Modal from './Modal'
+import RecipeCard from './RecipeCard'
+import EditCard from './EditCard'
+import store from './store'
+
+// Material Design palette
+// https://material.io/color/#!/?view.left=0&view.right=0&primary.color=3F51B5&secondary.color=FFEB3B
 
 class App extends Component {
-    constructor () {
-        super()
-        this.recipeStore = new Store()
-    }
-
     render() {
         return (
             <Router>
                 <div className="App">
                     <div className="App-header">
-                        <h2>Recipe Box</h2>
+                        <h1>Recipe Box</h1>
                     </div>
-                    <RecipeList />
-                    <Route path='/:recipe' render={props => (
-                        <RecipeModal {...props} />
-                    )} />
-                    <Route path='/:recipe/edit' render={props => (
-                        <EditModal {...props} />
-                    )} />
+                        <RecipeList recipes={store.selectAll()} />
+                        <Route path='/:recipe' render={props => (
+                            <Modal {...props} >
+                                <RecipeCard {...props} store={store} />
+                            </Modal>
+                        )} />
+                        <Route path='/:recipe/edit' render={props => (
+                            <Modal {...props} >
+                                <EditCard {...props} store={store} />
+                            </Modal>
+                        )} />
                 </div>
             </Router>
         )
