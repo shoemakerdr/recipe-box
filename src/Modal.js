c17a32c91a8539ef693ab5d84fba6ce7ddcb40ff
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-14 21:01:16 +0000

    Added Modal component

---
+++ src/Modal.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+
+class Modal extends Component {
+    constructor (props) {
+        super(props)
+        this.redirectHome = this.redirectHome.bind(this)
+        this.state = {
+            shouldRedirect: false
+        }
+    }
+
+    redirectHome (event) {
+        if (event.target.id === 'Modal') {
+            this.setState({shouldRedirect: true})
+        }
+    }
+
+    render () {
+        const { shouldRedirect } = this.state
+        return (
+            <div id='Modal' onClick={this.redirectHome} className='Modal modal-wrapper'>
+                <div className='modal-card'>
+                    {this.props.children}
+                </div>
+                {shouldRedirect &&
+                    <Redirect to='/' />}
+            </div>
+        )
+    }
+}
+export default Modal
