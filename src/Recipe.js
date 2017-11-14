4cefb3917554b17da8282a956eb413beea4c032b
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-14 21:10:14 +0000

    Removed Recipe component. Changed styling. Added Link from react-router to /:recipe route.

--- src/Recipe.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-const Recipe = props => 
-    <div>{props.recipeName}</div>
-    
-export default Recipe
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,12 +1,55 @@
-import React, { Component } from 'react'
-import Recipe from './Recipe'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-class RecipeList extends Component {
-    render () {
-        return (
-            <Recipe key='1' recipeName={'Chicken and rice'} />   
-        )
+const styles = {
+    list: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    recipe: {
+        backgroundColor: '#757de8',
+        borderRadius: '8px',
+        margin: '16px 64px',
+        padding: '4px 160px',
+        color: 'white',
+    },
+    create: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ffeb3b',
+        width: '64px',
+        height: '64px',
+        marginTop: '-32px',
+        borderRadius: '100%',
     }
+}
+
+const RecipeList = props => {
+    return (
+        <div style={styles.list}>
+            <Link to='/'>
+                <div style={styles.create}>
+                    <h1>+</h1>
+                </div>
+            </Link>
+            {props.recipes.length ?
+                <div>
+                    {props.recipes.map(recipe => {
+                        return (
+                            <Link key={recipe.id} to={`/${recipe.id}`}>
+                                <div style={styles.recipe}>
+                                    <h2>{recipe.name}</h2>
+                                </div>
+                            </Link>
+                        )
+                    })}
+                </div>
+                : 'No recipes yet.'}
+        </div>
+    )
 }
 
 export default RecipeList
