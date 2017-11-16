a0cea31cb6177eecd5b73fbcbb8590ed28ff946e
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-16 22:15:55 +0000

    Added extra data

--- src/store-config.js
+++ src/store-config.js
@@ -16,6 +16,38 @@ const defaultRecipes = [
         name: 'Noodles & Tomatoes',
         ingredients: ['noodles','crushed tomatoes','salt']
     },
+    {
+        name: 'Spaghetti & Meatballs',
+        ingredients: ['sauce','noodles', 'meatballs', 'parmesan']
+    },
+    {
+        name: 'Chicken & Rice',
+        ingredients: ['chicken breast','salt','pepper','rice','yellow rice seasoning']
+    },
+    {
+        name: 'Mahkani Chicken',
+        ingredients: ['chicken breast', 'tomatoes','butter','garam masala','cream','yogurt','cumin']
+    },
+    {
+        name: 'Noodles & Tomatoes',
+        ingredients: ['noodles','crushed tomatoes','salt']
+    },
+    {
+        name: 'Spaghetti & Meatballs',
+        ingredients: ['sauce','noodles', 'meatballs', 'parmesan']
+    },
+    {
+        name: 'Chicken & Rice',
+        ingredients: ['chicken breast','salt','pepper','rice','yellow rice seasoning']
+    },
+    {
+        name: 'Mahkani Chicken',
+        ingredients: ['chicken breast', 'tomatoes','butter','garam masala','cream','yogurt','cumin']
+    },
+    {
+        name: 'Noodles & Tomatoes',
+        ingredients: ['noodles','crushed tomatoes','salt']
+    },
 ]
 
 export function setupStore (store) {
