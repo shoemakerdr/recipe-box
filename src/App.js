5fd8b4c8cb954629969ab75ab152e1df23f29600
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-16 17:17:12 +0000

    Added keyup event to close modals on escape press. - Added escapePressed boolean field to state. - Added componentDidMount method to register keyup listener. - Added componentDidUpdate method to revert escapePressed state back to false. - Passed escapePressed to Modal instances as props.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import './App.css'
 import RecipeList from './RecipeList'
 import Modal from './Modal'
@@ -12,7 +12,29 @@ import store from './store'
 // https://material.io/color/#!/?view.left=0&view.right=0&primary.color=3F51B5&secondary.color=FFEB3B
 
 class App extends Component {
+    constructor () {
+        super()
+        this.state = {
+            escapePressed: false,
+        }
+    }
+
+    componentDidMount () {
+        document.addEventListener('keyup', event => {
+            if (event.keyCode === 27 && window.location.pathname !== '/') {
+                this.setState({escapePressed: true})
+            }
+        })
+    }
+
+    componentDidUpdate () {
+        if (this.state.escapePressed) {
+            this.setState({escapePressed: false})
+        }
+    }
+
     render() {
+        const { escapePressed } = this.state
         return (
             <Router>
                 <div className="App">
@@ -21,18 +43,20 @@ class App extends Component {
                     </div>
                         <RecipeList recipes={store.selectAll()} />
                         <Route path='/new' render={props => (
-                            <Modal {...props} >
+                            <Modal escapePressed={escapePressed} {...props} >
                                 <NewCard {...props} store={store} />
+                                {this.shouldRedirect && <Redirect to='/' />}
                             </Modal>
                         )} />
                         <Route path='/recipes/:recipe' render={props => (
-                            <Modal {...props} >
+                            <Modal escapePressed={escapePressed} {...props} >
                                 <RecipeCard {...props} store={store} />
                             </Modal>
                         )} />
                         <Route path='/edit/:recipe' render={props => (
-                            <Modal {...props} >
+                            <Modal escapePressed={escapePressed} {...props} >
                                 <EditCard {...props} store={store} />
+                                {this.shouldRedirect && <Redirect to='/' />}
                             </Modal>
                         )} />
                 </div>
