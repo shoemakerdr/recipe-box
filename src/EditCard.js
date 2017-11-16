6e2ed9005522c1523686d40f0953d7e71f00cfc8
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-16 17:29:01 +0000

    Added form elements and empty methods for future form logic

--- src/EditCard.js
+++ src/EditCard.js
@@ -9,10 +9,67 @@ class EditCard extends Component {
         }
     }
 
+    handleSubmit () {}
+
+    nameChange () {}
+
+    ingredientChange () {}
+
+    deleteIngredient (index) {}
+
+    hasNoEmptyInputs () {}
+
+    canSubmit () {}
+
+    redirectToRecipe (id) {}
+
     render () {
+        const { name, id, ingredients } = this.state.recipe
         return (
-            <form>
-                <div>Edit recipe #{this.props.match.params.recipe}</div>
+            <form className='EditCard' onSubmit={this.handleSubmit}>
+                <h1>Edit Your Recipe</h1>
+                <input
+                    className='input'
+                    placeholder='Recipe Name'
+                    type="text"
+                    value={name}
+                    onChange={this.nameChange}
+                />
+                { ingredients.map((ingredient, i) => {
+                    return (
+                        <div key={i}>
+                            <input
+                                className='input'
+                                placeholder='Ingredient'
+                                type="text"
+                                name={`${i}`}
+                                value={ingredient}
+                                onChange={this.ingredientChange}
+                            />
+                            <button
+                                className='button'
+                                type='button'
+                                onClick={() => this.deleteIngredient(i)}
+                            >
+                                X
+                            </button>
+                        </div>
+                    )
+                })}
+                <button
+                    className={this.hasNoEmptyInputs() ? 'button' : 'disabled-button'}
+                    type='button'
+                    disabled={!this.hasNoEmptyInputs()}
+                    onClick={this.addIngredient}
+                >
+                    Add Ingredient
+                </button>
+                <input
+                    className={this.canSubmit() ? 'button' : 'disabled-button'}
+                    type="submit"
+                    disabled={!this.canSubmit()}
+                    value="Submit" />
+                {this.redirectToRecipe(id)}
             </form>
         )
     }
