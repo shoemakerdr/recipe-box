c3728c4da926fccc31cdc45076403dd517fad9ad
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-16 22:22:38 +0000

    Removed styles object

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-const styles = {
-    button: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#ffeb3b',
-        width: '64px',
-        height: '64px',
-    }
-}
-
 class RecipeCard extends Component {
     constructor (props) {
         super(props)
