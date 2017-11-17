caefda3f8fc98a55776a09fa33955623e0cbd1a4
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-17 22:21:20 +0000

    NewCard now extends FormCard and overwrites handleSubmit method

--- src/NewCard.js
+++ src/NewCard.js
@@ -1,69 +1,27 @@
-import React, { Component } from 'react'
+import FormCard from './FormCard'
+import store from './store'
 
-class EditCard extends Component {
+class NewCard extends FormCard {
     constructor (props) {
         super(props)
+        this.handleSubmit = this.handleSubmit.bind(this)
         this.defaultState = {
+            title: 'Create A New Recipe',
             name: '',
             ingredients: ['']
         }
         this.state = this.defaultState
     }
-
-    handleSubmit () {}
     
-    nameChange () {}
-    
-    ingredientChange () {}
-    
-    hasNoEmptyInputs () {}
-    
-    canSubmit () {}
-    
-    redirectToRecipe (id) {}
-
-    render () {
-        return (
-            <div className='EditCard'>
-                <header><h1>Create A New Recipe</h1></header>
-                <form className='card-content' onSubmit={this.handleSubmit}>
-                    <input
-                        className='input'
-                        placeholder='Recipe Name'
-                        type="text"
-                        value={this.state.name}
-                        onChange={this.nameChange}
-                    />
-                    { this.state.ingredients.map((ingredient, i) => {
-                        return (
-                            <input
-                                className='input'
-                                placeholder='Ingredient'
-                                type="text"
-                                key={i}
-                                name={`${i}`}
-                                value={this.state.ingredients[i]}
-                                onChange={this.ingredientChange}
-                            />
-                        )
-                    })}
-                    <button
-                        className={this.hasNoEmptyInputs() ? 'button' : 'disabled-button'}
-                        type='button'
-                        disabled={!this.hasNoEmptyInputs()}
-                        onClick={this.addIngredient}
-                    >
-                        Add Ingredient
-                    </button>
-                    <input
-                        className={this.canSubmit() ? 'button' : 'disabled-button'}
-                        type="submit"
-                        disabled={!this.canSubmit()}
-                        value="Save" />
-                    {this.redirectToRecipe()}
-                </form>
-            </div>
-        )
+    handleSubmit (event) {
+        event.preventDefault()
+        const recipe = {
+            name: this.state.name,
+            ingredients: this.state.ingredients,
+        }
+        const id = store.insert(recipe)
+        this.redirectLink = `/recipes/${id}`
+        this.setState({shouldRedirect: true})
     }
 }
-export default EditCard
+export default NewCard
